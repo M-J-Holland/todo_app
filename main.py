b175c65d80a6848122ec3f546d8c7661e7c5bb2d
mathew <mathew_holland@hotmail.co.uk>
2026-02-02 20:27:26 +0000

    Added a show feature to display todos. Added comments for both show and add features.

--- main.py
+++ main.py
@@ -5,6 +5,16 @@ todos = []
 while True:
     #get user input
     user_input = input(f"{message}: ").lower().strip()
+
+    # Add feature
     if user_input == 'add' or user_input.startswith('a'):
         todo = input("Enter a todo to add: ")
         todos.append(todo)
+    # Show feature
+    elif user_input == "show":
+        if not todos:
+            print("No todos to show, add some first them come back to see them.")
+
+        else:
+            for index, todo in enumerate(todos, start=1):
+                print(f"{index}. {todo}")
